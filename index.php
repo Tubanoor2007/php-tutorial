a37537865758d6d1a294dd31998c0d340022c4d6
Tubanoor2007 <tubaq343>
2024-09-23 17:50:52 +0530

    msg

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>hello</h1>
+<?php
+
+var_dump(5);
+echo "<br>";
+var_dump("john");
+echo "<br>";
+var_dump(3.14);
+echo "<br>";
+var_dump(true);
+echo "<br>";
+var_dump([2,3,56]);
+echo "<br>";
+
+$t=$r=$s="fruit";
+echo $t;
+echo $r;
+echo $s;
+echo "<br>";
+print "<h1>php is fun</h1>";
+print "<h2>i ove php</h2>";
+
+
+?>
+
+</body>
+</html>
